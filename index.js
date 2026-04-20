799004fe9a405c71833c13467757e467fb8f6c82
Fatma Ince <zmfrstr@outlook.com>
2026-04-20 03:01:23 +0300

    1. gorev yapıldı

--- index.js
+++ index.js
@@ -50,9 +50,12 @@ function KareninAlani(kenaruzunlugu) {
 	4. Hesaplanan çemberin çevresi döndürülecektir.
 */
 
-function CemberinCevresi(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinCevresi(yaricap) {
+  let cevre = 2 * pi * yaricap; 
+  return cevre;
 }
+
+console.log(CemberinCevresi(5));
 
 /* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
 
